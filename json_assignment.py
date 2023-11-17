09585f6c0c3bf6c34d6ec05422de61c372f5aa20
Sai790 <149860240+Sai790@users.noreply.github.com>
2023-11-17 09:46:36 +0530

    Update json_assignment.py
    changed the json_assignment

--- json_assignment.py
+++ json_assignment.py
@@ -11,4 +11,7 @@ for inter in intern_data['interns']:
         email=(f"{inter['first_name']}-{inter['middle_name']}.{inter['last_name']}@pvpsit.com")
         name=(f"{inter['first_name']}{inter['middle_name']}{inter['last_name']}")
     diction[email]={"id":inter["id"], "fullname":name} 
-print(diction)
+
+with open ("output.json","w") as s3:
+     s4 = json.dump(diction,s3,indent=3)
+print(s4)
